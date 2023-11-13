0d153173f3a1c9129f6a7e4b49eaaa2e6c38ce0a
Loic Barthomeuf Ippon <lbarthomeuf@ippon.fr>
2023-11-13 16:38:04 +0100

    avant todo list

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 import http from 'http'
+import fs from 'fs'
 
 const server = http.createServer(maCallback)
 
@@ -8,17 +9,26 @@ server.listen(3000, () => {
 
 function maCallback(request, response) {
   // SERVEUR DES PAGES INDEX ET FORM :
+  const { method, url } = request
 
-  
-  //   let data = ''
-  //   if (request.method == 'POST') {
-  //     request.on('data', function (chunk) {
-  //       console.log('Received data:', chunk.toString())
-  //       data += chunk.toString()
-  //     })
-  //     request.on('end', function () {
-  //       console.log('Complete data:', data)
-  //       response.end('received')
-  //     })
-  //   }
+  if (method === 'GET') {
+    const adress = url === '/form' ? 'form' : 'index'
+    const htmlPage = fs.readFileSync(`./public/${adress}.html`)
+    response.setHeader('Content-Type', 'text/html')
+    response.end(htmlPage)
+  }
+
+  let data = ''
+  if (request.method == 'POST') {
+    request.on('data', function (chunk) {
+      data += chunk.toString()
+    })
+    request.on('end', function () {
+      const parsedData = JSON.parse(data)
+      console.log(parsedData.name, 'connected')
+
+      response.writeHead(201)
+      response.end('received')
+    })
+  }
 }
