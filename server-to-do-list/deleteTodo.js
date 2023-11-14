1fd6b02622e1a6e21b8abf541316257aa705cf5a
Loic Barthomeuf Ippon <lbarthomeuf@ippon.fr>
2023-11-14 11:24:16 +0100

    fin du chapitre 1

--- server-to-do-list/deleteTodo.js
+++ server-to-do-list/deleteTodo.js
@@ -1,3 +1,5 @@
+import fs from "fs";
+
 export function deleteTodo(req, res) {
   let data = "";
   req.on("data", function (chunk) {
@@ -5,9 +7,30 @@ export function deleteTodo(req, res) {
   });
   req.on("end", function () {
     const body = JSON.parse(data);
-    console.log(body);
-
+    deleteOneTodoBDD(body.todo);
     res.writeHead(201);
-    res.end("received");
+    res.end("deleted");
   });
+
+  function deleteOneTodoBDD(todoToDelete) {
+    const todoListBuffer = fs.readFileSync("items.txt");
+    const todoListString = todoListBuffer.toString();
+    const todoListArray = todoListString.split("\n");
+    const purgedToDolist = todoListArray.filter((item) => item);
+
+    const elementInd = purgedToDolist.findIndex((toDo) => todoToDelete == toDo);
+    purgedToDolist.splice(elementInd, 1);
+
+    if (elementInd === -1) {
+      res.writeHead(404);
+      res.end("Todo non trouvé");
+      return;
+    }
+
+    let newTodoList = "";
+    purgedToDolist.forEach((element) => {
+      newTodoList = newTodoList + element + "\n";
+    });
+    fs.writeFileSync("items.txt", newTodoList);
+  }
 }
