d8a12cc47dbb7519b66126a397871cc86ffc5e30
Loic Barthomeuf Ippon <lbarthomeuf@ippon.fr>
2023-11-28 10:22:12 +0100

    multer fin

--- multer/index.ts
+++ multer/index.ts
@@ -1,1 +1,14 @@
-console.log("ok");
+import express from "express";
+import multer from "multer";
+
+const upload = multer({ dest: "uploads/" });
+const app = express();
+
+app.use("/", express.static("public"));
+
+app.post("/api", upload.single("avatar"), (req, res) => {
+  console.log(req.body);
+  res.status(201).json("enregistré");
+});
+
+app.listen(3000);
