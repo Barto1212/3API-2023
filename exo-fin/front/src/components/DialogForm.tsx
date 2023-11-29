3301e4c2068f92ab71d46b8d9e5e5e283bb6de3e
Loic Barthomeuf Ippon <lbarthomeuf@ippon.fr>
2023-11-29 10:59:57 +0100

    add post front

--- exo-fin/front/src/components/DialogForm.tsx
+++ exo-fin/front/src/components/DialogForm.tsx
@@ -1,13 +1,26 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import { TextField } from "@mui/material";
+import { useState, ChangeEvent } from "react";
+import { NewPost } from "../../../types";
+import { postPost } from "../utils/api";
+
+const emptyPost: NewPost = {
+  title: "",
+  description: "",
+  date: new Date(),
+  img: "",
+  author: "",
+  likes: 0,
+};
 
 export default function DialogForm() {
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = useState(false);
+  const [postFormValue, setPostFormValue] = useState<NewPost>(emptyPost);
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -16,11 +29,22 @@ export default function DialogForm() {
   const handleClose = () => {
     setOpen(false);
   };
+  const handleSend = () => {
+    postPost(postFormValue).then(() => {
+      handleClose();
+    });
+  };
+  const setFormValue = (
+    e: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
+  ) => {
+    const { name } = e.target;
+    setPostFormValue((old) => ({ ...old, [name]: e.target.value }));
+  };
 
   return (
-    <React.Fragment>
+    <>
       <Button variant="outlined" onClick={handleClickOpen}>
-        Open alert dialog
+        Ajouter un post
       </Button>
       <Dialog
         open={open}
@@ -28,22 +52,49 @@ export default function DialogForm() {
         aria-labelledby="alert-dialog-title"
         aria-describedby="alert-dialog-description"
       >
-        <DialogTitle id="alert-dialog-title">
-          {"Use Google's location service?"}
-        </DialogTitle>
+        <DialogTitle id="alert-dialog-title">Ajouter un post</DialogTitle>
         <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            Let Google help apps determine location. This means sending anonymous
-            location data to Google, even when no apps are running.
-          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            name="title"
+            label="Titre"
+            type="text"
+            fullWidth
+            variant="standard"
+            value={postFormValue.title}
+            onChange={setFormValue}
+          />
+          <TextField
+            autoFocus
+            margin="dense"
+            name="description"
+            label="Description"
+            type="text"
+            fullWidth
+            variant="standard"
+            value={postFormValue.description}
+            onChange={setFormValue}
+          />
+          <TextField
+            autoFocus
+            margin="dense"
+            name="img"
+            label="Lien image"
+            type="text"
+            fullWidth
+            variant="standard"
+            value={postFormValue.img}
+            onChange={setFormValue}
+          />
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleClose}>Disagree</Button>
-          <Button onClick={handleClose} autoFocus>
-            Agree
+          <Button onClick={handleClose}>Annuler</Button>
+          <Button onClick={handleSend} autoFocus>
+            Envoyer
           </Button>
         </DialogActions>
       </Dialog>
-    </React.Fragment>
+    </>
   );
 }
--- exo-fin/front/src/utils/api.ts
+++ exo-fin/front/src/utils/api.ts
@@ -1,8 +1,12 @@
 import axios from "axios";
-import { Post } from "../../../types";
+import { NewPost, Post } from "../../../types";
 
 export const getPosts: () => Promise<Post[]> = async () => {
   const posts = (await axios.get<Post[]>("http://localhost:5000/post")) ;
+  return posts.data;
+};
 
+export const postPost: (newPost: NewPost) => Promise<string> = async (newPost) => {
+  const posts = (await axios.post<string>("http://localhost:5000/post",newPost)) ;
   return posts.data;
 };
