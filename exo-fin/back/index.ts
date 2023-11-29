a6e975648aba0bfe46607f96c983b6a8aed1a97f
Loic Barthomeuf Ippon <lbarthomeuf@ippon.fr>
2023-11-29 10:20:56 +0100

    cors

--- exo-fin/back/index.ts
+++ exo-fin/back/index.ts
@@ -2,10 +2,12 @@ import express from "express";
 import { connectDB } from "./utils/connectDB";
 import { getPost } from "./controlers/getPost";
 import "dotenv/config";
+import cors from "cors";
 
 connectDB();
 
 const app = express();
+app.use(cors());
 
 app.get("/post", getPost);
 
--- exo-fin/front/src/App.tsx
+++ exo-fin/front/src/App.tsx
@@ -1,21 +1,31 @@
 import { Container, Stack } from "@mui/material";
 import Post from "./components/Post";
+import { useEffect } from "react";
+import { getPosts } from "./utils/api";
 
-const App = () => (
-  <Container sx={{ width: "100%" }}>
-    <Stack spacing={5} direction="column" alignItems="center">
-      <Post
-        img="https://mui.com/static/images/cards/contemplative-reptile.jpg"
-        title="Lizard"
-        _id="2"
-        description="Lizards are a widespread group of squamate reptiles, with over 6,000
+const App = () => {
+  useEffect(() => {
+    console.log("ok");
+    getPosts().then((response) => {
+      console.log(response);
+    });
+  }, []);
+  return (
+    <Container sx={{ width: "100%" }}>
+      <Stack spacing={5} direction="column" alignItems="center">
+        <Post
+          img="https://mui.com/static/images/cards/contemplative-reptile.jpg"
+          title="Lizard"
+          _id="2"
+          description="Lizards are a widespread group of squamate reptiles, with over 6,000
             species, ranging across all continents except Antarctica"
-        date={new Date()}
-        author="LB"
-        likes={3}
-      />
-    </Stack>
-  </Container>
-);
+          date={new Date()}
+          author="LB"
+          likes={3}
+        />
+      </Stack>
+    </Container>
+  );
+};
 
 export default App;
