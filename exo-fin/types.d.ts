6e66043df2c2ba4a1e4e87b9091a5f244138cf92
Loic Barthomeuf Ippon <lbarthomeuf@ippon.fr>
2023-11-29 09:27:24 +0100

    types

--- exo-fin/types.d.ts
+++ exo-fin/types.d.ts
@@ -1,8 +1,12 @@
-export type Post = {
+export interface NewPost {
     title: string,
     description: string,
     date: Date,
     img: string,
     author: string,
     likes: number
+}
+
+export interface Post extends NewPost {
+    _id: string
 }
